a0b4a101646f24e0c97871a3a81a16bdd86a8fe4
Mert Celikan <52200813+mertcelikan@users.noreply.github.com>
2023-10-06 17:10:42 +0300

    Update main.py

--- dbconverter/main.py
+++ dbconverter/main.py
@@ -1,17 +1,16 @@
 from converter import Converter
 
 # DW bilgieri
-DATAWAREHOUSE_HOST = "172.16.5.16"
+DATAWAREHOUSE_HOST = ""
 DATAWAREHOUSE_USER = "bi-admin"
-DATAWAREHOUSE_PASSWORD = "Dk5Bnqgj3gEsRz2p"
+DATAWAREHOUSE_PASSWORD = ""
 port_number = 3308
 
 
-uri = "mongodb+srv://mert:Mert123456!@cluster0.smovknl.mongodb.net/?retryWrites=true&w=majority"
-mysql_bilgilerim = {'host': DATAWAREHOUSE_HOST, 'user': DATAWAREHOUSE_USER, 'password': DATAWAREHOUSE_PASSWORD, 'port': port_number, 'database': 'weg_hotel'}
+uri = "mongodb+srv://admin:admin@cluster0.smovknl.mongodb.net/?retryWrites=true&w=majority"
+mysql_bilgilerim = {'host': DATAWAREHOUSE_HOST, 'user': DATAWAREHOUSE_USER, 'password': DATAWAREHOUSE_PASSWORD, 'port': port_number, 'database': 'your_db_name'}
 
 my_converter = Converter(uri, **mysql_bilgilerim)
-my_converter.mysql_to_mongodb('hotels_pro_b2b_test')
-#my_converter.mongodb_to_mysql('hotels_pro_b2b_test')
+my_converter.mysql_to_mongodb('test_table')
 
 
